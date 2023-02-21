85ad7558f3bc1fbd6f94deb825e24f3e5ad36c78
Perla Elizabeth Aguilar Maldonado <perlaelizabeth@MacBook-Pro-de-Perla.local>
2023-02-21 11:14:40 -0600

    Prettier

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,3 @@
 export default function LoginPage() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
 }
