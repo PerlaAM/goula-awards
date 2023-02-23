202794286f62fc6bf511762d1669d6eaab6b5d14
Perla Elizabeth Aguilar Maldonado <perlaelizabeth@MacBook-Pro-de-Perla.local>
2023-02-22 22:01:09 -0600

    Home and login relocated

--- app/page.jsx
+++ app/page.jsx
@@ -1,173 +1,51 @@
-"use client";
+const fetchContents = () => {
+  return fetch("https://awards.goula.lat/cms/wp-json/wp/v2/convocatoria/49")
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    });
+};
 
-import React, { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
-import axios from "axios";
-import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
-import ErrorAlert from "./components/errorAlert";
-import SuccessAlert from "./components/successAlert";
-
-export default function LoginPage() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const [isLoading, setIsLoading] = useState(false);
-  const [isHideError, setIsHideError] = useState(true);
-  const [isHideSuccess, setIsHideSuccess] = useState(true);
-  const url = "https://tusitioweb.dev/awards/wp-json/jwt-auth/v1/token";
-  const registerStatus = searchParams.get("status");
-
-  useEffect(() => {
-    if (registerStatus === "registered") {
-      setIsHideSuccess(false);
-    } else {
-      setIsHideSuccess(true);
-    }
-  }, []);
-
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({
-    mode: "onChange",
-  });
-
-  const formOnSubmit = (loginInformation) => {
-    setIsLoading(true);
-    setIsHideError(true);
-    setIsHideSuccess(true);
-
-    let newLoginInformation = {
-      username: loginInformation.email,
-      password: loginInformation.password,
-    };
-
-    doLoginRequest(newLoginInformation);
-  };
-
-  const doLoginRequest = (newLoginInformation) => {
-    axios
-      .post(url, newLoginInformation)
-      .then(() => {
-        router.push("/home");
-      })
-      .catch(function (error) {
-        setIsLoading(false);
-        setIsHideError(false);
-      });
-  };
-
-  function LoadingText() {
-    if (isLoading) {
-      return "Iniciando sesión";
-    }
-    return "Iniciar sesión";
-  }
+export default async function HomePage({}) {
+  const contents = await fetchContents();
 
   return (
-    <section className="flex h-full items-center justify-center bg-rosy">
-      <div className="mx-14 md:mx-32 w-3/4 md:w-2/4 lg:w-1/4">
-        <ErrorAlert
-          isHide={isHideError}
-          title={"Ha ocurrido un error"}
-          description={
-            "No pudimos encontrar una cuenta con esa dirección de correo electrónico"
-          }
-        ></ErrorAlert>
-        <SuccessAlert
-          isHide={isHideSuccess}
-          title={"Se ha creado el registro con éxito"}
-          description={"Inicia sesión con tu correo y contraseña"}
-        ></SuccessAlert>
-        <h1 className="mb-8 text-center text-rose text-2xl">Iniciar sesión</h1>
-        <form onSubmit={handleSubmit(formOnSubmit)}>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="mb-1 block text-sm font-normal text-rose"
-            >
-              Correo electrónico
-            </label>
-            <input
-              id="email"
-              type="email"
-              name="email"
-              autoComplete="off"
-              className="block w-full rounded-lg bg-white py-2 px-3 text-black focus:outline-none focus:border-rose focus:ring-rose focus:ring-2 text-sm"
-              {...register("email", {
-                required: "Este campo es requerido",
-                pattern: {
-                  value:
-                    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                  message: "Correo electrónico no válido",
-                },
-              })}
-            />
-            {errors.email && (
-              <p className="text-xs pt-2 pb-1 text-pink break-after-column">
-                {errors.email.message}
-              </p>
-            )}
+    <section className="bg-white">
+      <div className="py-36 h-full mx-auto w-9/12 md:max-w-3xl lg:max-w-6xl lg:px-12 text-center">
+        <h1 className="mb-6 font-display text-4xl font-light tracking-tighter text-rose sm:text-6xl">
+          {contents.acf.titulo}
+        </h1>
+        <span
+          className="mt-6 space-y-6 font-display font-extralight text-xl tracking-tight text-rose"
+          dangerouslySetInnerHTML={{ __html: contents.acf.descripcion }}
+        ></span>
+      </div>
+      <div className="h-full bg-rosy">
+        <div className="py-28 h-full mx-auto w-9/12 md:max-w-3xl lg:max-w-6xl lg:px-12">
+          <div className="sm:columns-2 ">
+            <div className="py-4 sm:pr-8">
+              <h1 className="mb-8 font-display text-4xl font-light tracking-tighter text-rose sm:text-4xl">
+                {contents.acf.bases.subtitulo}
+              </h1>
+              <p className="text-black">{contents.acf.bases.contenido}</p>
+            </div>
+            <div className="py-4 sm:pl-8 break-before-column mt-20 md:mt-0">
+              <h1 className="mb-8 font-display text-4xl font-light tracking-tighter text-rose sm:text-4xl">
+                {contents.acf.fechas_importantes.subtitulo}
+              </h1>
+              {contents.acf.fechas_importantes.fechas.map((data) => (
+                <div className="mb-9">
+                  <p className="text-xl font-semibold text-rose">
+                    {data.fecha.split(":")[0]}
+                  </p>
+                  <p className="uppercase text-3xl leading-none text-dark-green">
+                    {data.fecha.split(":")[1]}
+                  </p>
+                </div>
+              ))}
+            </div>
           </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="mb-1 block text-sm font-normal text-rose"
-            >
-              Contraseña
-            </label>
-            <input
-              id="password"
-              type="password"
-              name="password"
-              maxLength="8"
-              className="block w-full rounded-lg bg-white py-2 px-3 text-black focus:outline-none focus:border-rose focus:ring-rose focus:ring-2 text-sm"
-              {...register("password", {
-                required: "Este campo es requerido",
-                pattern: {
-                  value:
-                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d$@$!%*?&]{8}$/,
-                  message:
-                    "La contraseña debe tener al menos una minúscula, una mayúscula, un número",
-                },
-              })}
-            />
-            {errors.password && (
-              <p className="text-xs pt-2 pb-1 text-pink">
-                {errors.password.message}
-              </p>
-            )}
-          </div>
-
-          <button
-            type="submit"
-            className={`flex items-center justify-center rounded-lg py-2 px-3 text-sm font-light transition-colors bg-rose text-yellow mt-8 w-full ${
-              !isLoading ? "hover:bg-dark-rose" : "opacity-60"
-            }`}
-            type="submit"
-            disabled={isLoading ? "disabled" : ""}
-          >
-            <img
-              src="my-loader.svg"
-              alt="Cargando..."
-              className={`mr-2 ${!isLoading ? "hidden" : ""}`}
-            />
-            <LoadingText />
-          </button>
-
-          <div className="mt-4">
-            <p className="text-black font-light text-sm text-center">
-              ¿No tienes una cuenta?
-              <Link
-                href="/register"
-                className="text-pink font-normal hover:text-dark-pink ml-1"
-              >
-                Crear cuenta
-              </Link>
-            </p>
-          </div>
-        </form>
+        </div>
       </div>
     </section>
   );
